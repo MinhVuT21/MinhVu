841f411457dfcac1cb3e985ade3b329d5d76a4b6
MinhVuT21 <90442209+ToMinhVu@users.noreply.github.com>
2021-10-29 11:34:50 +0700

    first commit

---
+++ lt.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int a, b, temp;
+	printf("Nhap phuong trinh ax2+bx+c=0\n");
+	printf("Nhap a: ");
+	scanf("%d", &a);
+	printf("Nhap b: ");
+	scanf("%d", &b);
+	
+	temp = a;
+	a = b;
+	b = temp;
+
+	printf("\na = %d, b = %d", a, b);
+		
+		
+		
+	
+	
+	return 0;
+}
